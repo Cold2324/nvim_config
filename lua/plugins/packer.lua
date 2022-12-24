f6366e15f41333f9c5f15d017dd5e03a31471d1d
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-24 17:51:59 -0500

    treesitter plugin added

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -55,6 +55,15 @@ return require('packer').startup(function(use)
   use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
   use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
+  -- Treesitter
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
