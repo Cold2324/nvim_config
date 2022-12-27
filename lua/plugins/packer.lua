9568dea7583c456d9a97a9554e002f01626027e3
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-26 20:30:55 -0500

    web-devicons plugin added

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   } 
+  use 'nvim-tree/nvim-web-devicons'
 
   -- Utilities
   use "windwp/nvim-autopairs"
