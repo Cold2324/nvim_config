2c60b32c022dfe665a0198c72399dee935c92881
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-26 21:31:30 -0500

    colorful plugin added

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,7 +1,5 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
+  local fn = vim.fn local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim' if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
     return true
@@ -72,6 +70,10 @@ return require('packer').startup(function(use)
   }
 
   use "lukas-reineke/indent-blankline.nvim"
+
+  use {
+    "nvim-zh/colorful-winsep.nvim",
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
