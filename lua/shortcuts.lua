4ff9676beaf2eb6e3e34a5cb5139b7aa3143c31d
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-29 19:43:54 -0500

    add shortcut to comment-box plugin

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -1,28 +1,40 @@
 vim.g.mapleader = ' '
 
--- function
-function map(mode, shortcut, command)
+--  +-----------+
+--  | functions |
+--  +-----------+
+local function map(mode, shortcut, command)
   vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
 end
 
-function nmap(shortcut, command)
+local function nmap(shortcut, command)
   map('n', shortcut, command)
 end
 
-function imap(shortcut, command)
-  map('i', shortcut, command)
+local function vmap(shortcut, command)
+  map('v', shortcut, command)
 end
 
--- shortcuts
+--  +-----------------+
+--  | Basic Shortcuts |
+--  +-----------------+
 nmap('<leader>w', ':w<CR>')
 nmap('<leader>q', ':q<CR>')
 nmap('<leader>fi', '$')
 nmap('<leader>in', '0')
 nmap('<leader>t', 'ciw')
-map('v', '<', '<gv')
-map('v', '>', '>gv')
+nmap('<leader>te', ':terminal ')
 nmap('<leader>v', ':vsplit<CR>')
+vmap('<', '<gv')
+vmap('>', '>gv')
 
--- Neotree
+--  +---------+
+--  | Neotree |
+--  +---------+
 nmap('<leader>e', ':NeoTreeFloat<CR>')
 nmap('<leader>c', ':NeoTreeClose<CR>')
+
+--  +-------------+
+--  | Comment Box |
+--  +-------------+
+nmap('<leader>box', ':CBacbox10<CR>')
